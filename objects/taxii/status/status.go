67e980a02d8d341c6451ccf20b26ac76ac48ea65
黄承开 <chinky.huang@dbappsecurity.com.cn>
2020-10-27 11:12:47 +0800

    :bug: Fix(): status
    Signed-off-by: 黄承开 <chinky.huang@dbappsecurity.com.cn>

--- objects/taxii/status/status.go
+++ objects/taxii/status/status.go
@@ -200,7 +200,7 @@ CreateSuccessDetails - This method will create a SuccessDetails type based on th
 data provided and will add it to the Successes property.
 */
 func (o *Status) CreateSuccessDetails(id, ver, mesg string) error {
-	s, _ := r.NewSuccessDetails()
+	s, _ := o.NewSuccessDetails()
 	s.SetID(id)
 	s.SetVersion(ver)
 	s.SetMessage(mesg)
@@ -223,9 +223,9 @@ AddFailureDetails - This method will take in a SuccessDetails type as a pointer
 and add it to the Failures property. One would use this if they manually created
 their own SuccessDetails type and then wanted to add it to the Failures property.
 */
-func (o *Status) AddFailureDetails(o *StatusDetails) (int, error) {
+func (o *Status) AddFailureDetails(sd *StatusDetails) (int, error) {
 	positionThatAppendWillUse := len(o.Failures)
-	o.Failures = append(o.Failures, *o)
+	o.Failures = append(o.Failures, *sd)
 	return positionThatAppendWillUse, nil
 }
 
@@ -248,8 +248,8 @@ to the Pendings property, and return a pointer to that instance.
 func (o *Status) NewPendingDetails() (*StatusDetails, error) {
 	obj := NewStatusDetails()
 	positionThatAppendWillUse := len(o.Pendings)
-	o.Pendings = append(r.Pendings, *obj)
-	return &r.Pendings[positionThatAppendWillUse], nil
+	o.Pendings = append(o.Pendings, *obj)
+	return &o.Pendings[positionThatAppendWillUse], nil
 }
 
 /*
@@ -257,9 +257,9 @@ AddPendingDetails - This method will take in a SuccessDetails type as a pointer
 and add it to the Pendings property. One would use this if they manually created
 their own SuccessDetails type and then wanted to add it to the Pendings property.
 */
-func (o *Status) AddPendingDetails(o *StatusDetails) (int, error) {
+func (o *Status) AddPendingDetails(sd *StatusDetails) (int, error) {
 	positionThatAppendWillUse := len(o.Pendings)
-	o.Pendings = append(o.Pendings, *o)
+	o.Pendings = append(o.Pendings, *sd)
 	return positionThatAppendWillUse, nil
 }
 
@@ -268,7 +268,7 @@ CreatePendingDetails - This method will create a SuccessDetails type based on th
 data provided and will add it to the Pending property.
 */
 func (o *Status) CreatePendingDetails(id, ver, mesg string) error {
-	s, _ := r.NewPendingDetails()
+	s, _ := o.NewPendingDetails()
 	s.SetID(id)
 	s.SetVersion(ver)
 	s.SetMessage(mesg)
